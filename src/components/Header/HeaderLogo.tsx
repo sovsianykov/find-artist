c1815af68963d617ded1bab6b6506e8b4814a5b7
”sovsianykov” <swebproger@gmail.com>
2021-10-31 21:33:18 +0200

    initial commit2

---
+++ src/components/Header/HeaderLogo.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+
+const HeaderLogo:FunctionComponent = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HeaderLogo;
